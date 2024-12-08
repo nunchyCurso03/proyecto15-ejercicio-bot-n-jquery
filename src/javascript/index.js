fa98c9cf54b49dbe9b69a4175b3eeae58c15e570
curso03 <tuemail@example.com>
2024-12-08 17:25:06 +0100

    4º: Lista: : meter dinámicamente en una lista los elementos de un array

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,4 +1,4 @@
- $(document).ready(function () {
+$(document).ready(function () {
     $("#boton1").click(function () {
         alert("Alguien ha pulsado el botón"); // alerta cuando se pulsa el botón
     });
@@ -9,16 +9,41 @@
 
 
 
-    
+
     $("#boton3").click(function () {
-        var nuevoBoton3= $('<button id="nuevoBoton3">¡Ta Ta Ta....chán!</button>');
+        let nuevoBoton3 = $('<button id="nuevoBoton3">¡Ta Ta Ta....chán!</button>');
         $("#nuevoBoton3Contenedor").append(nuevoBoton3);
     });
 
 
-}); 
-
- 
 
 
- 
+    $("#boton4").click(function () {
+        let hdsMiembros = [{
+            "nombre": "Enrique Bunbury",
+        }, {
+            "nombre": "Juan Valdivia",
+        }, {
+            "nombre": "Pedro Andreu",
+        }, {
+            "nombre": "Joaquín Cardiel",
+        }];
+
+        for (i = 0 ; i < hdsMiembros.length; i++ ) {
+            $("#heroesDelSilencioLista").append(
+                $("<li>")
+                    .text(hdsMiembros[i].nombre)                                      
+            );
+        }
+
+    });
+
+
+
+
+
+});
+
+
+
+
